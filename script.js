81ad72e8bbf9f5a124f814a713071ac481387c0a
danlakelake <danlakelake@gmail.com>
2025-01-08 17:11:51 -0600

    siguiendo con la funcionalidad en archivo js

--- script.js
+++ script.js
@@ -8,4 +8,11 @@ const ticketPrice = parseInt(movieSelect.value);
 
 console.log(typeof ticketPrice);
 
+container.addEventListener('click', function(e){
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        console.log(e.target);
+        e.target.classList.toggle('selected');
+    }
+});
+
 
